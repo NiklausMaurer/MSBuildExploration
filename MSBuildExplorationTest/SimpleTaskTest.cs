200727da8f1e92c759e8b0b3bf52f3ae26abd419
nim <niklaus.maurer@m-s.ch>
2016-04-04 16:36:55 +0200

    Adds SimpleTaskTest

--- MSBuildExplorationTest/SimpleTaskTest.cs
+++ MSBuildExplorationTest/SimpleTaskTest.cs
@@ -1,13 +1,33 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSBuildExploration;
 
 namespace MSBuildExplorationTest
 {
 	[TestClass]
 	public class SimpleTaskTest
 	{
-		[TestMethod]
-		public void TestMethod1()
+		#region Properties
+		private SimpleTask SimpleTask { get; set; }
+		#endregion
+
+		#region Initialize and Cleanup
+		[TestInitialize]
+		public void InitializeTest()
 		{
+			this.SimpleTask = new SimpleTask();
 		}
+		#endregion
+
+		#region Tests
+		[TestMethod]
+		public void TestExecution()
+		{
+			// Act
+			bool bSuccess = this.SimpleTask.Execute();
+
+			// Assert
+			Assert.IsTrue(bSuccess);
+		}
+		#endregion
 	}
 }
