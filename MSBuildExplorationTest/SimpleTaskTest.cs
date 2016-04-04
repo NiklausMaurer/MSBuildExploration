afb4cf8d670c52696dc9ba830ea9a967d73b7937
nim <niklaus.maurer@m-s.ch>
2016-04-04 17:46:35 +0200

    Adds serious unit testing

--- MSBuildExplorationTest/SimpleTaskTest.cs
+++ MSBuildExplorationTest/SimpleTaskTest.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Build.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MSBuildExploration;
+using NSubstitute;
 
 namespace MSBuildExplorationTest
 {
@@ -8,13 +10,18 @@ namespace MSBuildExplorationTest
 	{
 		#region Properties
 		private SimpleTask SimpleTask { get; set; }
+		private IBuildEngine BuildEngine { get; set; }
 		#endregion
 
 		#region Initialize and Cleanup
 		[TestInitialize]
 		public void InitializeTest()
 		{
-			this.SimpleTask = new SimpleTask();
+			this.BuildEngine = Substitute.For<IBuildEngine>();
+			this.SimpleTask = new SimpleTask
+			                  {
+				                  BuildEngine = this.BuildEngine
+			                  };
 		}
 		#endregion
 
@@ -22,10 +29,15 @@ namespace MSBuildExplorationTest
 		[TestMethod]
 		public void TestExecution()
 		{
+			// Arrange
+			this.SimpleTask.Name = "Michael Jackson";
+
 			// Act
 			bool bSuccess = this.SimpleTask.Execute();
 
 			// Assert
+			Assert.IsFalse(this.SimpleTask.Log.HasLoggedErrors);
+			this.BuildEngine.Received().LogMessageEvent(Arg.Is<BuildMessageEventArgs>(args => args.Message.Contains("Michael Jackson")));
 			Assert.IsTrue(bSuccess);
 		}
 		#endregion
