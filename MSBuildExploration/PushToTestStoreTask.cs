3ca47585c30ba43afb3b849bf7d438a4bf279e6c
Niklaus Maurer <nick.is.normal@gmail.com>
2016-04-05 13:30:05 +0200

    ADDS: PushToTestStoreTask

---
+++ MSBuildExploration/PushToTestStoreTask.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Utilities;
+
+namespace MSBuildExploration
+{
+    public class PushToTestStoreTask : Task
+    {
+        public override bool Execute()
+        {
+            return true;
+        }
+    }
+}
---
+++ MSBuildExplorationTest/PushToTestStoreTask.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSBuildExploration;
+using NSubstitute;
+
+namespace MSBuildExplorationTest
+{
+    [TestClass]
+    public class PushToTestStoreTaskTest
+    {
+        #region Properties
+        private PushToTestStoreTask PushToTestStoreTask { get; set; }
+        private IBuildEngine BuildEngine { get; set; }
+        #endregion
+
+        #region Initialize and Cleanup
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            this.BuildEngine = Substitute.For<IBuildEngine>();
+            this.PushToTestStoreTask = new PushToTestStoreTask
+            {
+                BuildEngine = this.BuildEngine
+            };
+        }
+        #endregion
+
+        #region Tests
+        [TestMethod]
+        public void TestExecution()
+        {
+            // Act
+            bool bSuccess = this.PushToTestStoreTask.Execute();
+
+            // Assert
+            Assert.IsFalse(this.PushToTestStoreTask.Log.HasLoggedErrors);
+            Assert.IsTrue(bSuccess);
+        }
+        #endregion
+    }
+}
